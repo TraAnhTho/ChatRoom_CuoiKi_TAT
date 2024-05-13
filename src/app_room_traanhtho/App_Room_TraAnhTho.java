619990863ce6de74e29c17d28034634fa9c38c03
TraAnhTho <thotra081105@gmail.com>
2024-05-13 08:30:52 +0700

    feat: build success base and layoutview

---
+++ src/app_room_traanhtho/App_Room_TraAnhTho.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package app_room_traanhtho;
+
+/**
+ *
+ * @author PC
+ */
+public class App_Room_TraAnhTho {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
